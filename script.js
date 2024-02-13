19d33d5a27a8d10b181e17aea789b305011272cf
169398 <kulubiidris@gmail.com>
2024-02-14 01:38:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const  info = document.querySelectorAll('.info li');
+console.log(info);
+const about = document.querySelector('.about');
+const lightColorArr= ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink', 'brown', 'grey', 'black'];
+
+function shuffleArray(arr){
+    let currentIndex = arr.length;
+    let randomIndex;
+
+    while (currentIndex !==0){
+        randomIndex = math.floor(math.random() * currentIndex);
+        currentIndex--;
+        [arr[currentIndex], arr[randomIndex],arr[randomIndex],];
+    }
+    return arr;
+}
+ 
+
+about.addEventListener('click',()=>{
+    const shuffledColors = shuffleArray(lightColorArr);
+
+
+    about.forEach((list,index)=>{
+        list.style.backgroundColor = shuffledColors[index];
+    });
+});
