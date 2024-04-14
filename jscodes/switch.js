0d6f63b2bd42dabefcdd6ae7e695aaaa6e3f405f
169398 <kulubiidris@gmail.com>
2024-04-14 23:17:36 +0300

    switch statements

---
+++ jscodes/switch.js
@@ -0,0 +1,20 @@
+//This is the  switch statement
+//syntax
+
+
+let b=2 +5;
+switch(b){
+
+    case 3:
+    alert('Too small buddy');
+    break;
+
+    case 4:
+        alert('Still too small');
+        case 7:
+            alert('That it big man');
+            default:
+                alert('No such values exist');
+
+
+}
